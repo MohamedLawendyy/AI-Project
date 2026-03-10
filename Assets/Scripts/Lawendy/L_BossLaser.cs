d3f817336b26b364920b6655a1ee8ff1e6fe46df
Mohamed Lawendy <mohamed.h.lawendy@gmail.com>
2026-03-10 12:01:57 +0200

    Testing

---
+++ Assets/Scripts/Lawendy/L_BossLaser.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class L_BossLaser : MonoBehaviour
+{
+    public VisualEffect laserVFX;
+    public Transform laserOrigin;
+
+    public float maxLength = 20f;
+
+    void Update()
+    {
+        Ray ray = new Ray(laserOrigin.position, laserOrigin.forward);
+        RaycastHit hit;
+
+        float length = maxLength;
+
+        if (Physics.Raycast(ray, out hit, maxLength))
+        {
+            length = hit.distance;
+
+            if (hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Player Hit");
+            }
+        }
+
+        laserVFX.SetFloat("BeamLength", length);
+    }
+
+    public void StartLaser()
+    {
+        laserVFX.SetBool("FireLaser", true);
+    }
+
+    public void StopLaser()
+    {
+        laserVFX.SetBool("FireLaser", false);
+    }
+}
